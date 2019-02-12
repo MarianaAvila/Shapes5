d33b2f1c2ca6c53ff76f0f0e931825496b6dac99
csPython <cspython@AD25TF04NGF1J-61.local>
2019-02-12 11:53:50 -0500

    2 shapes corner screen

--- MAshapes5.py
+++ MAshapes5.py
@@ -1,9 +1,13 @@
 from graphics import*
 
-bTriWin =GraphWin ("Blue Triangle",300,300)
-bTriWin.setCoords (0,0,300,300)
-bTri = Polygon(Point(100,100),Point(150,200), Point(200,100))
-bTri.setFill(color_rgb(30,30,230))
-bTri.draw(bTriWin)
-bTriWin.getmouse()
+shapesWin = GraphWin("Shapes",500,500)
+shapesWin.setCoords(0,0,500,500)
 
+bTri = Polygon(Point(10,50),Point(50,100), Point(150,100))
+bTri.setFill(color_rgb(30,30,230))
+bTri.draw(shapesWin)
+
+
+rSquare = Polygon(Point(50,450),Point(150,450),Point(150,350),Point(50,350))
+rSquare.setFill(color_rgb(178,34,34))               
+rSquare.draw(shapesWin)
