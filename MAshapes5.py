606bb5c5b9f27f0a48b1a0b127af65e5a43bd2e8
csPython <cspython@AD25TF04NGF1J-61.local>
2019-02-15 09:56:23 -0500

    Added Pentagon and started Diamond

--- MAshapes5.py
+++ MAshapes5.py
@@ -1,5 +1,8 @@
 from graphics import*
 
+xWindowSize = 500
+yWindowSize = 500 
+
 shapesWin = GraphWin("Shapes",500,500)
 shapesWin.setCoords(0,0,500,500)
 
@@ -8,6 +11,17 @@ bTri.setFill(color_rgb(30,30,230))
 bTri.draw(shapesWin)
 
 
-rSquare = Polygon(Point(50,450),Point(150,450),Point(150,350),Point(50,350))
+rSquare = Rectangle(Point(50,350),Point(150,450))
 rSquare.setFill(color_rgb(178,34,34))               
 rSquare.draw(shapesWin)
+
+oCircle= Circle(Point(440,440),50)
+oCircle.setFill(color_rgb(255,69,0))
+oCircle.draw(shapesWin)
+
+gPentagon = Polygon(Point(370,80),Point(370,130),Point(395,180),Point(420,130),Point(420,80))
+gPentagon.setFill(color_rgb(50,205,50)) 
+gPentagon.draw(shapesWin)
+
+#bDiamond = Polygon(Point((xWindowSize/2)),((yWindowSize/2+50))
+    
